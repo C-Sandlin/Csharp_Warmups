5dc6025242c18ab751bd6c4af767f60a515b7c83
Colin Sandlin <csandlin010@gmail.com>
2019-07-10 10:17:36 -0500

    added fields and constructors

--- Program.cs
+++ Program.cs
@@ -15,7 +15,7 @@ namespace warmups
                 Speed = 0.79
             };
 
-            var airplane2 = new Airplane()
+            var airplane2 = new Airplane(10)
             {
                 Make = "Airbus",
                 Model = "A320",
@@ -29,21 +29,47 @@ namespace warmups
             };
 
             airplane1.Fly();
-            airplane2.Fly();
+            airplane2.Fly(2500);
+            // after below line is run, mileage for airplane2 will be added to be 5000
+            airplane2.Fly(2500);
 
         }
     }
 
     class Airplane
     {
+        // private means anywhere in this class, I can modify miles, but can't modify outside class.
+        // called a field - fields are private, start with an underscore, are lowercase camelcase
+        private int _miles = 0;
+
+        // all below are called properties.
         public string Make { get; set; }
         public string Model { get; set; }
         public int NumberSeats { get; set; }
         public double Speed { get; set; }
 
+        // CONSTRUCTORS
+        public Airplane(int startingMiles)
+        {
+            _miles = startingMiles;
+        }
+
+        public Airplane()
+        {
+
+        }
+
+        // METHODS
         public void Fly()
         {
             Console.WriteLine($"The {Make} {Model} has {NumberSeats} seats and flys at Mach {Speed}.");
+        }
+
+        // Overloading with a field "milesDriven"
+        public void Fly(int milesDriven)
+        {
+            _miles += milesDriven;
+            Console.WriteLine($"The {Make} {Model} has {NumberSeats} seats and flys at Mach {Speed}. This one has now flown {_miles} miles");
         }
     }
 }
