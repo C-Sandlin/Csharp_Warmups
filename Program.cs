79b527720901b9bcf77020390ca580025f6111a1
Colin Sandlin <csandlin010@gmail.com>
2019-07-10 09:16:39 -0500

    finished warmup 1

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace warmups
 {
@@ -6,7 +7,45 @@ namespace warmups
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var airplane1 = new Airplane()
+            {
+                Make = "Boeing",
+                Model = "737 Max",
+                Seats = 230,
+                Speed = 0.79
+            };
+
+            var airplane2 = new Airplane()
+            {
+                Make = "Airbus",
+                Model = "A320",
+                Seats = 198,
+                Speed = 0.81
+            };
+
+            var Hangar = new List<Airplane>()
+            {
+                airplane1, airplane2
+            };
+
+            airplane1.Fly();
+            airplane2.Fly();
+
+        }
+    }
+
+    class Airplane
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Seats { get; set; }
+        public double Speed { get; set; }
+
+        public void Fly()
+        {
+            Console.WriteLine($"The {Make} {Model} has {Seats} seats and flys at Mach {Speed}.");
         }
     }
 }
+
+// make model # of seats
